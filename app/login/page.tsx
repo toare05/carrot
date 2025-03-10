d6e2a670c05e59dc7217dc455cb8834369092d26
toare05 <toarete05@gmail.com>
2025-03-10 16:32:47 +0900

    5.1 3:30

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,13 +3,18 @@ import FormInput from "@/components/form-input";
 import SocialLogin from "@/components/social-login";
 
 export default function LogIn() {
+  async function handForm(){
+    // Handle form submission
+    "use server";
+    console.log("Form submitted");
+  };
   return (
     <div className="flex flex-col gap-10 py-8 px-6">
       <div className="flex flex-col gap-2 *:font-medium">
         <h1 className="text-2xl">안녕하세요!</h1>
         <h2 className="text-xl">Log in with email and password.</h2>
       </div>
-      <form className="flex flex-col gap-3">
+      <form action={handForm} className="flex flex-col gap-3">
         <FormInput type="email" placeholder="Email" required errors={[]} />
         <FormInput
           type="password"
